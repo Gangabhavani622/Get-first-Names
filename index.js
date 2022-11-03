a0984f82949ba578d18fa35a5474b5ac589fae73
Gangabhavani622 <115251057+Gangabhavani622@users.noreply.github.com>
2022-11-03 15:19:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const people_names = require("../country/state/city/index");
 const utility = require("../utilities/utils/index");
 
-function getPeopleInCity() {
+function getPeopleInCity(people_names) {
   let firstNames = utility(people_names);
   return firstNames;
 }
